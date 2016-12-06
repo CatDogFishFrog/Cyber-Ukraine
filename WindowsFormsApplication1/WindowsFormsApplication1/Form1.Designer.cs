1047b1e240a8b33380439af8a82643f75eeb8801
Андрей <@DADDY>
2016-12-06 18:59:09 +0200

    Создан предварительный макет программы

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,71 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Map = new System.Windows.Forms.TableLayoutPanel();
             this.Col_Variable = new System.Windows.Forms.NumericUpDown();
-            this.Ebat = new System.Windows.Forms.Button();
+            this.Unit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.меню1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.меню2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.менюnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Col_Variable)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // Map
             // 
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(360, 69);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(194, 195);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.Map.ColumnCount = 5;
+            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Map.Location = new System.Drawing.Point(12, 118);
+            this.Map.Name = "Map";
+            this.Map.RowCount = 5;
+            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Map.Size = new System.Drawing.Size(220, 210);
+            this.Map.TabIndex = 0;
             // 
             // Col_Variable
             // 
-            this.Col_Variable.Location = new System.Drawing.Point(12, 12);
+            this.Col_Variable.Location = new System.Drawing.Point(12, 51);
             this.Col_Variable.Name = "Col_Variable";
             this.Col_Variable.Size = new System.Drawing.Size(75, 20);
             this.Col_Variable.TabIndex = 1;
             this.Col_Variable.ValueChanged += new System.EventHandler(this.Col_Variable_ValueChanged);
             this.Col_Variable.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
             // 
-            // Ebat
+            // Unit
             // 
-            this.Ebat.Location = new System.Drawing.Point(12, 38);
-            this.Ebat.Name = "Ebat";
-            this.Ebat.Size = new System.Drawing.Size(75, 23);
-            this.Ebat.TabIndex = 2;
-            this.Ebat.Text = "Склеить";
-            this.Ebat.UseVisualStyleBackColor = true;
+            this.Unit.Location = new System.Drawing.Point(12, 77);
+            this.Unit.Name = "Unit";
+            this.Unit.Size = new System.Drawing.Size(75, 23);
+            this.Unit.TabIndex = 2;
+            this.Unit.Text = "Склеить";
+            this.Unit.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(93, 14);
+            this.label1.Location = new System.Drawing.Point(9, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Количество переменных";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.меню1ToolStripMenuItem,
+            this.меню2ToolStripMenuItem,
+            this.менюnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(243, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // меню1ToolStripMenuItem
+            // 
+            this.меню1ToolStripMenuItem.Name = "меню1ToolStripMenuItem";
+            this.меню1ToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.меню1ToolStripMenuItem.Text = "Меню 1";
+            // 
+            // меню2ToolStripMenuItem
+            // 
+            this.меню2ToolStripMenuItem.Name = "меню2ToolStripMenuItem";
+            this.меню2ToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.меню2ToolStripMenuItem.Text = "Меню 2";
+            // 
+            // менюnToolStripMenuItem
+            // 
+            this.менюnToolStripMenuItem.Name = "менюnToolStripMenuItem";
+            this.менюnToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.менюnToolStripMenuItem.Text = "Меню \"n\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Карта Карно";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 327);
+            this.ClientSize = new System.Drawing.Size(243, 364);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Ebat);
+            this.Controls.Add(this.Unit);
             this.Controls.Add(this.Col_Variable);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.Map);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
-            this.Text = "Karno";
+            this.Text = "Минимизация карты Карно";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Col_Variable)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,10 +154,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel Map;
         private System.Windows.Forms.NumericUpDown Col_Variable;
-        private System.Windows.Forms.Button Ebat;
+        private System.Windows.Forms.Button Unit;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem меню1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem меню2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem менюnToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
     }
 }
 
