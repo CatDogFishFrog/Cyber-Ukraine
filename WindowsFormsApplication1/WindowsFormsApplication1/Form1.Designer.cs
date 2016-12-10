5f20990eb940391dca00efb8bf867f0dd0c2a063
Андрей <@DADDY>
2016-12-10 13:16:44 +0200

    Динамическое изменение таблицы v1

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Map = new System.Windows.Forms.TableLayoutPanel();
             this.Col_Variable = new System.Windows.Forms.NumericUpDown();
             this.Unit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -37,38 +36,35 @@
             this.меню2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.менюnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label2 = new System.Windows.Forms.Label();
+            this.Map = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.Col_Variable)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Map
-            // 
-            this.Map.ColumnCount = 5;
-            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.Map.Location = new System.Drawing.Point(12, 118);
-            this.Map.Name = "Map";
-            this.Map.RowCount = 5;
-            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.Map.Size = new System.Drawing.Size(220, 210);
-            this.Map.TabIndex = 0;
             // 
             // Col_Variable
             // 
             this.Col_Variable.Location = new System.Drawing.Point(12, 51);
+            this.Col_Variable.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.Col_Variable.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.Col_Variable.Name = "Col_Variable";
-            this.Col_Variable.Size = new System.Drawing.Size(75, 20);
+            this.Col_Variable.Size = new System.Drawing.Size(33, 20);
             this.Col_Variable.TabIndex = 1;
+            this.Col_Variable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Col_Variable.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.Col_Variable.ValueChanged += new System.EventHandler(this.Col_Variable_ValueChanged);
-            this.Col_Variable.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
             // 
             // Unit
             // 
@@ -87,7 +83,6 @@
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Количество переменных";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // menuStrip1
             // 
@@ -99,7 +94,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuStrip1.Size = new System.Drawing.Size(243, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(905, 24);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -130,15 +125,27 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Карта Карно";
             // 
+            // Map
+            // 
+            this.Map.AllowUserToAddRows = false;
+            this.Map.AllowUserToDeleteRows = false;
+            this.Map.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Map.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Map.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Map.Location = new System.Drawing.Point(12, 106);
+            this.Map.Name = "Map";
+            this.Map.Size = new System.Drawing.Size(841, 421);
+            this.Map.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 327);
+            this.ClientSize = new System.Drawing.Size(905, 536);
+            this.Controls.Add(this.Map);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Unit);
             this.Controls.Add(this.Col_Variable);
-            this.Controls.Add(this.Map);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Минимизация карты Карно";
@@ -146,14 +153,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.Col_Variable)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel Map;
         private System.Windows.Forms.NumericUpDown Col_Variable;
         private System.Windows.Forms.Button Unit;
         private System.Windows.Forms.Label label1;
@@ -162,6 +168,11 @@
         private System.Windows.Forms.ToolStripMenuItem меню2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem менюnToolStripMenuItem;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Map;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C4;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,16 +16,172 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void Col_Variable_ValueChanged(object sender, EventArgs e)
         {
-
-        }
-
-        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            
-            
+            string @var = Col_Variable.Text;
+           switch (@var)
+            {
+                case "2":
+                    Map.Rows.Clear();
+                    while(Map.Columns.Count !=0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "0");
+                    Map.Columns.Add("Col2", "1");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Rows.Add(2);
+                    Map.Rows[0].HeaderCell.Value = "0";
+                    Map.Rows[1].HeaderCell.Value = "1";
+                    break;
+                case "3":
+                    Map.Rows.Clear();
+                    while (Map.Columns.Count != 0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "00");
+                    Map.Columns.Add("Col2", "01");
+                    Map.Columns.Add("Col3", "11");
+                    Map.Columns.Add("Col4", "10");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Columns[2].Width = 50;
+                    Map.Columns[3].Width = 50;
+                    Map.Rows.Add(2);
+                    Map.Rows[0].HeaderCell.Value = "0";
+                    Map.Rows[1].HeaderCell.Value = "1";
+                    break;
+                case "4":
+                    Map.Rows.Clear();
+                    while (Map.Columns.Count != 0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "00");
+                    Map.Columns.Add("Col2", "01");
+                    Map.Columns.Add("Col3", "11");
+                    Map.Columns.Add("Col4", "10");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Columns[2].Width = 50;
+                    Map.Columns[3].Width = 50;
+                    Map.Rows.Add(4);
+                    Map.Rows[0].HeaderCell.Value = "00";
+                    Map.Rows[1].HeaderCell.Value = "01";
+                    Map.Rows[2].HeaderCell.Value = "11";
+                    Map.Rows[3].HeaderCell.Value = "10";
+                    break;
+                case "5":
+                    Map.Rows.Clear();
+                    while (Map.Columns.Count != 0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "000");
+                    Map.Columns.Add("Col2", "001");
+                    Map.Columns.Add("Col3", "011");
+                    Map.Columns.Add("Col4", "010");
+                    Map.Columns.Add("Col5", "110");
+                    Map.Columns.Add("Col6", "111");
+                    Map.Columns.Add("Col7", "101");
+                    Map.Columns.Add("Col8", "100");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Columns[2].Width = 50;
+                    Map.Columns[3].Width = 50;
+                    Map.Columns[4].Width = 50;
+                    Map.Columns[5].Width = 50;
+                    Map.Columns[6].Width = 50;
+                    Map.Columns[7].Width = 50;
+                    Map.Rows.Add(4);
+                    Map.Rows[0].HeaderCell.Value = "00";
+                    Map.Rows[1].HeaderCell.Value = "01";
+                    Map.Rows[2].HeaderCell.Value = "11";
+                    Map.Rows[3].HeaderCell.Value = "10";
+                    break;
+                case "6":
+                    Map.Rows.Clear();
+                    while (Map.Columns.Count != 0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "000");
+                    Map.Columns.Add("Col2", "001");
+                    Map.Columns.Add("Col3", "011");
+                    Map.Columns.Add("Col4", "010");
+                    Map.Columns.Add("Col5", "110");
+                    Map.Columns.Add("Col6", "111");
+                    Map.Columns.Add("Col7", "101");
+                    Map.Columns.Add("Col8", "100");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Columns[2].Width = 50;
+                    Map.Columns[3].Width = 50;
+                    Map.Columns[4].Width = 50;
+                    Map.Columns[5].Width = 50;
+                    Map.Columns[6].Width = 50;
+                    Map.Columns[7].Width = 50;
+                    Map.Rows.Add(8);
+                    Map.Rows[0].HeaderCell.Value = "000";
+                    Map.Rows[1].HeaderCell.Value = "001";
+                    Map.Rows[2].HeaderCell.Value = "011";
+                    Map.Rows[3].HeaderCell.Value = "010";
+                    Map.Rows[4].HeaderCell.Value = "110";
+                    Map.Rows[5].HeaderCell.Value = "111";
+                    Map.Rows[6].HeaderCell.Value = "101";
+                    Map.Rows[7].HeaderCell.Value = "100";
+                    break;
+                case "7":
+                    Map.Rows.Clear();
+                    while (Map.Columns.Count != 0)
+                    {
+                        Map.Columns.RemoveAt(0);
+                    }
+                    Map.Columns.Add("Col1", "0000");
+                    Map.Columns.Add("Col2", "0001");
+                    Map.Columns.Add("Col3", "0011");
+                    Map.Columns.Add("Col4", "0010");
+                    Map.Columns.Add("Col5", "0110");
+                    Map.Columns.Add("Col6", "0111");
+                    Map.Columns.Add("Col7", "0101");
+                    Map.Columns.Add("Col8", "0100");
+                    Map.Columns.Add("Col1", "1100");
+                    Map.Columns.Add("Col2", "1101");
+                    Map.Columns.Add("Col3", "1111");
+                    Map.Columns.Add("Col4", "1110");
+                    Map.Columns.Add("Col5", "1010");
+                    Map.Columns.Add("Col6", "1011");
+                    Map.Columns.Add("Col7", "1001");
+                    Map.Columns.Add("Col8", "1000");
+                    Map.Columns[0].Width = 50;
+                    Map.Columns[1].Width = 50;
+                    Map.Columns[2].Width = 50;
+                    Map.Columns[3].Width = 50;
+                    Map.Columns[4].Width = 50;
+                    Map.Columns[5].Width = 50;
+                    Map.Columns[6].Width = 50;
+                    Map.Columns[7].Width = 50;
+                    Map.Columns[8].Width = 50;
+                    Map.Columns[9].Width = 50;
+                    Map.Columns[10].Width = 50;
+                    Map.Columns[11].Width = 50;
+                    Map.Columns[12].Width = 50;
+                    Map.Columns[13].Width = 50;
+                    Map.Columns[14].Width = 50;
+                    Map.Columns[15].Width = 50;
+                    Map.Rows.Add(8);
+                    Map.Rows[0].HeaderCell.Value = "000";
+                    Map.Rows[1].HeaderCell.Value = "001";
+                    Map.Rows[2].HeaderCell.Value = "011";
+                    Map.Rows[3].HeaderCell.Value = "010";
+                    Map.Rows[4].HeaderCell.Value = "110";
+                    Map.Rows[5].HeaderCell.Value = "111";
+                    Map.Rows[6].HeaderCell.Value = "101";
+                    Map.Rows[7].HeaderCell.Value = "100";
+                    break;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -33,10 +189,5 @@ namespace WindowsFormsApplication1
 
         }
 
-        private void Col_Variable_ValueChanged(object sender, EventArgs e)
-        {
-            char c;
-            
-        }
     }
 }
